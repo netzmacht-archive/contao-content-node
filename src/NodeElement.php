2c4a2b4eeb8976c6cf1a308353ce911ba3def346
David Molineus <david.molineus@netzmacht.de>
2015-03-06 18:05:27 +0100

    Commit node element.

---
+++ src/NodeElement.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * @package    dev
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2015 netzmacht creative David Molineus
+ * @license    LGPL 3.0
+ * @filesource
+ *
+ */
+
+namespace Netzmacht\Contao\NestedContent;
+
+class NodeElement extends \ContentElement
+{
+    protected $strTemplate = 'ce_node';
+
+    public function generate()
+    {
+        if (TL_MODE === 'BE') {
+            return $this->generateBackendView();
+        }
+
+        return parent::generate();
+    }
+
+    protected function compile()
+    {
+        $content  = '';
+        $elements = \ContentModel::findBy(
+            array('pid=?', 'ptable=?'),
+            array($this->id, 'tl_content_node')
+        );
+
+        if ($elements) {
+            foreach ($elements as $element) {
+                $content .= $this->getContentElement($element, $this->column);
+            }
+        }
+
+        $this->Template->content = $content;
+    }
+
+    private function generateBackendView()
+    {
+        $content  = '';
+        $callback = $GLOBALS['TL_DCA']['tl_content']['list']['sorting']['child_record_callback'];
+
+        if ($callback) {
+            $renderer = new $callback[0];
+            $elements = \ContentModel::findBy(
+                array('pid=?', 'ptable=?'),
+                array($this->id, 'tl_content_node')
+            );
+
+            if ($elements) {
+                foreach ($elements as $element) {
+                    $rendered = $renderer->{$callback[1]}($element->row());
+
+                    if ($rendered) {
+                        $content .= sprintf(
+                            '<div style="background: #fff;border-bottom: 4px solid #f3f3f3; padding: 6px">%s</div>',
+                            $rendered
+                        );
+                    }
+                }
+            }
+
+            return $content;
+        }
+    }
+}
